66489eb24494b03f0225501352c84408a9756e6c
50498 <50498@DESKTOP-KD0HG5K>
2021-04-26 00:10:12 -0600

    holixd

---
+++ src/main/java/com/curlp/capadatos/Conexion.java
@@ -0,0 +1,23 @@
+
+package com.curlp.capadatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private static String url = "jdbc:mysql://localhost:3306/inventario_master?zeroDateTimeBehavior=CONVERT_TO_NULL";
+    private static String user = "root";
+    private static String clave = "2Mas2son4.";
+    
+    public static Connection conectar() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); 
+            
+            //Obtener cadena de conexión.
+            return DriverManager.getConnection(url, user, clave);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException(e.getMessage());
+        }
+    }
+}
