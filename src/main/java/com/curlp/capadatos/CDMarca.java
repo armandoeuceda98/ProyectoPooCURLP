151151c2dbb26244f54cfbcc98c11622dfd330c4
Armando Euceda Molina <50498@DESKTOP-KD0HG5K>
2021-04-28 22:14:51 -0600

    Creación de Capa de Datos de Marca y Proveedor.

--- src/main/java/com/curlp/capadatos/CDMarca.java
+++ src/main/java/com/curlp/capadatos/CDMarca.java
@@ -1,6 +1,121 @@
 
 package com.curlp.capadatos;
 
+import com.curlp.capalogica.CLMarca;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
 public class CDMarca {
     
+    //Variables de conexión y consulta.
+    private final Connection cn;
+    PreparedStatement ps;
+    Statement st;
+    ResultSet rs;
+
+    public CDMarca(Connection cn) throws SQLException{
+        this.cn = Conexion.conectar();
+    }
+    
+    //Métodos para tabla Marca
+    public void insertarMarca(CLMarca cl) throws SQLException{
+        String sql = "{call sp_insertarMarca(?)}";
+        try {
+            ps = cn.prepareCall(sql);
+            ps.setString(1, cl.getNombre());
+            ps.execute();
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+    }
+    
+    public void actualizarMarca(CLMarca cl) throws SQLException{
+        String sql = "{call sp_actualizarMarca(?,?)}";
+        try {
+            ps = cn.prepareCall(sql);
+            ps.setInt(1, cl.getIdMarca());
+            ps.setString(2, cl.getNombre());
+            ps.execute();
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+    }
+    
+    public void eliminarMarca(CLMarca cl) throws SQLException{
+        String sql = "{call sp_eliminarMarca(?)}";
+        try {
+            ps = cn.prepareCall(sql);
+            ps.setInt(1, cl.getIdMarca());
+            ps.execute();
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+    }
+    
+    public int autoIncrementarIdMarca() throws SQLException{
+        
+        String sql = "{call sp_autoIncrementarIdMarca()}";
+        int idMarca = 0;
+        try {
+            st = cn.createStatement();
+            rs = st.executeQuery(sql);
+            rs.next();
+            
+            idMarca = rs.getInt("idMarca");
+            if(idMarca == 0){
+                idMarca = 1;
+            }
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+        return idMarca;
+    }
+    
+    public List<CLMarca> mostrarMarca() throws SQLException{
+        String sql = "{call sp_mostrarMarca()}";
+        
+        List<CLMarca> myList= null;
+        try {
+            st = cn.createStatement();
+            rs = st.executeQuery(sql);
+            
+            myList = new ArrayList<>();
+            
+            while(rs.next()){
+                CLMarca cl = new CLMarca();
+                
+                cl.setIdMarca(rs.getInt("idMarca"));
+                cl.setNombre(rs.getString("nombre"));
+                myList.add(cl);
+            }
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+        return myList;
+    }
+    
+    public List<String> cargarMarca() throws SQLException{
+        String sql = "{call sp_mostrarMarca()}";
+        
+        List<String> myList= null;
+        try {
+            st = cn.createStatement();
+            rs = st.executeQuery(sql);
+            
+            myList = new ArrayList<>();
+            myList.add("--Seleccione--");
+            while(rs.next()){
+                myList.add(rs.getString("nombre"));
+            }
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+        return myList;
+    }
 }
--- src/main/java/com/curlp/capadatos/CDProveedor.java
+++ src/main/java/com/curlp/capadatos/CDProveedor.java
@@ -1,6 +1,137 @@
 
 package com.curlp.capadatos;
 
+import com.curlp.capalogica.CLProveedor;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
 public class CDProveedor {
     
+    //Variables de conexión y consulta.
+    private final Connection cn;
+    PreparedStatement ps;
+    Statement st;
+    ResultSet rs;
+
+    public CDProveedor() throws SQLException{
+        this.cn = Conexion.conectar();
+    }
+    
+    //Métodos para tabla Proveedor.
+    public void insertarProveedor(CLProveedor cl) throws SQLException{
+        String sql = "{call sp_insertarProveedor(?,?,?,?,?)}";
+        
+        try {
+            ps = cn.prepareCall(sql);
+            ps.setString(1, cl.getNombreEmpresa());
+            ps.setString(2, cl.getTelefono());
+            ps.setString(3, cl.getCorreo());
+            ps.setString(4, cl.getNombreRepresentante());
+            ps.setString(5, cl.getTelefonoRepresentante());
+            ps.execute();
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+    }
+    
+    public void actualizarProveedor(CLProveedor cl) throws SQLException{
+        String sql = "{call sp_actualizarProveedor(?,?,?,?,?,?)}";
+        
+        try {
+            ps = cn.prepareCall(sql);
+            ps.setInt(1, cl.getCodProveedor());
+            ps.setString(2, cl.getNombreEmpresa());
+            ps.setString(3, cl.getTelefono());
+            ps.setString(4, cl.getCorreo());
+            ps.setString(5, cl.getNombreRepresentante());
+            ps.setString(6, cl.getTelefonoRepresentante());
+            ps.execute();
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+    }
+    
+    public void eliminarProveedor(CLProveedor cl) throws SQLException{
+        String sql = "{call sp_eliminarProveedor(?)}";
+        
+        try {
+            ps = cn.prepareCall(sql);
+            ps.setInt(1, cl.getCodProveedor());
+            ps.execute();
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+    }
+    
+    public int autoIncrementarCodProveedor() throws SQLException{
+        
+        String sql = "{call sp_autoIncrementarCodProveedor()}";
+        int codProveedor = 0;
+        try {
+            st = cn.createStatement();
+            rs = st.executeQuery(sql);
+            rs.next();
+            
+            codProveedor = rs.getInt("codProveedor");
+            if(codProveedor == 0){
+                codProveedor = 1;
+            }
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+        return codProveedor;
+    }
+    
+    public List<CLProveedor> mostrarProveedor() throws SQLException{
+        String sql = "{call sp_mostrarMarca()}";
+        
+        List<CLProveedor> myList= null;
+        try {
+            st = cn.createStatement();
+            rs = st.executeQuery(sql);
+            
+            myList = new ArrayList<>();
+            
+            while(rs.next()){
+                CLProveedor cl = new CLProveedor();
+                
+                cl.setCodProveedor(rs.getInt("codProveedor"));
+                cl.setNombreEmpresa(rs.getString("nombreEmpresa"));
+                cl.setTelefono(rs.getString("telefono"));
+                cl.setCorreo(rs.getString("correo"));
+                cl.setNombreRepresentante(rs.getString("nombreRepresentante"));
+                cl.setTelefonoRepresentante(rs.getString("telefonoRepresentante"));
+                myList.add(cl);
+            }
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+        return myList;
+    }
+    
+    public List<String> cargarProveedor() throws SQLException{
+        String sql = "{call sp_mostrarMarca()}";
+        
+        List<String> myList= null;
+        try {
+            st = cn.createStatement();
+            rs = st.executeQuery(sql);
+            
+            myList = new ArrayList<>();
+            myList.add("--Seleccione--");
+            
+            while(rs.next()){
+                myList.add(rs.getString("nombreEmpresa"));
+            }
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+        return myList;
+    }
 }
